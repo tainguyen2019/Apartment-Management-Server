c06a92dc7f74956c4b449c1e870fc780cb2a47f9
tainguyen2019 <tainguyen.uit@gmail.com>
2021-07-13 23:24:00 +0700

    chore: change demo data

--- controllers/reflect.controller.js
+++ controllers/reflect.controller.js
@@ -1,4 +1,5 @@
 const CustomError = require('../constants/CustomError');
+const positionModel = require('../models/position.model');
 const reflectModel = require('../models/reflect.model');
 const logger = require('../utils/logger');
 
@@ -14,7 +15,10 @@ const search = async (req, res, next) => {
     }
 
     if (req.department_id) {
-      searchParams.department_id = req.department_id;
+      const isManager = await positionModel.isManager(req.staff_id);
+      if (!isManager) {
+        searchParams.department_id = req.department_id;
+      }
     }
     const totalRecords = await reflectModel.count(searchParams);
     const offset = (page - 1) * pageSize;
