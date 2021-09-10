478e97e1108a161127c748a1f8a82654a81d3a9d
tainguyen2019 <tainguyen.uit@gmail.com>
2021-09-10 21:38:29 +0700

    fix: log error

--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,4 +1,7 @@
+const logger = require('../utils/logger');
+
 const errorHandling = (error, _, res, __) => {
+  logger.error(error);
   const message = error.message || 'Có lỗi xảy ra.';
   const statusCode = error.statusCode || 500;
   return res.status(statusCode).json({
